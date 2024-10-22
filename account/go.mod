ca28a1e48617d8b2dff692717ada038a4080ca93
Egor Ivanov <egorchima@gmail.com>
2024-10-22 23:06:17 +0300

    elasticsearch first add

--- account/go.mod
+++ account/go.mod
@@ -18,7 +18,9 @@ require (
 	github.com/go-openapi/spec v0.20.6 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/olivere/elastic/v7 v7.0.32 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/tools v0.1.12 // indirect
--- account/internal/server/config.go
+++ account/internal/server/config.go
@@ -1,17 +1,31 @@
 package server
 
-import "account/internal/storage"
+import (
+	"account/internal/storage"
+	"log"
+
+	"github.com/olivere/elastic/v7"
+)
 
 type Config struct {
 	BindAddress    string `toml:"bind_address"`
 	LogLevel       string `toml:"log_level"`
 	LogHeaders     bool   `toml:"log_headers"`
-	LogBody        bool   `toml:"log_headers"`
-	LogQueryParams bool   `toml:"log_headers"`
+	LogBody        bool   `toml:"log_body"`
+	LogQueryParams bool   `toml:"log_query_params"`
 	StorageConfig  *storage.Config
+	ElasticClient  *elastic.Client
 }
 
 func NewConfig() *Config {
+	client, err := elastic.NewClient(
+		elastic.SetURL("http://elasticsearch:9200"),
+		elastic.SetBasicAuth("elastic", "password"),
+	)
+	if err != nil {
+		log.Fatal("Elasticsearch ElasticClient was not created")
+	}
+
 	return &Config{
 		BindAddress:    ":8081",
 		LogLevel:       "debug",
@@ -19,5 +33,6 @@ func NewConfig() *Config {
 		LogBody:        true,
 		LogQueryParams: true,
 		StorageConfig:  storage.NewConfig(),
+		ElasticClient:  client,
 	}
 }
